3461853be965069b50129d12fd5f133e79428357
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-09 14:28:43 +0100

    we have created a function that finds the area of ​​a rectangle

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sum (a,b){
-    console.log (a + b);
+    console.log ( a + b);
 }
-let s = sum (5, 3);
+sum(5,6);
+sum(6,47);
+
+//функция которая находит площадь прямоугольника
+
+function area(c,d){
+  console.log (c * d);
+}
+area(15,20);
 
