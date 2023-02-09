ee042c93a60d475f548701910edd9a1a3efbc6aa
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-09 14:40:26 +0100

    solved a problem with a FUNCTION WHICH DISPLAYS THIS NUMBER IS EVEN OR NOT

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ sum(5, 6);
 sum(6, 47);
 
 //функция которая находит площадь прямоугольника
-
 function area(c, d) {
     console.log("площадь прямоугольника равна " +  c * d);
 }
 area(15, 20);
 
+
+//число четное или не четное
+function  number(v){
+ if (v % 2 === 0){
+    console.log (v + " even");
+ } 
+ else if( v % 2 === 1){
+    console.log( v + " odd");
+ }
+ else{
+    console.log( v + " is not a number");
+ }
+
+}
+number( "tea");
